ebd5ccad90d6b4eb6721b4162ed4168086420523
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2022-02-08 22:01:16 +0300

    Add detecting of collision between mainGameObject and gameObjects

---
+++ Assets/Scripts/Components/TriggerDetectionComponent.cs
@@ -0,0 +1,47 @@
+﻿using Managers;
+using UnityEngine;
+using Views;
+
+namespace Components
+{
+    [RequireComponent(typeof(Collider))]
+    public class TriggerDetectionComponent : BaseComponent
+    {
+        public override void Initialize()
+        {
+        }
+        public override void UnInitialize()
+        {
+        }
+
+        public override void Subscribe()
+        {
+        }
+        public override void UnSubscribe()
+        {
+        }
+
+        private void OnTriggerEnter(Collider otherCollider)
+        {
+            if (CollisionDetectionManager.Instance == null)
+            {
+                return;
+            }
+
+            var view1 = this.gameObject.GetComponent<IView>();
+            var view2 = otherCollider.gameObject.GetComponent<IView>();
+            CollisionDetectionManager.Instance.HandleOnTriggerEnter(view1, view2);
+        }
+        private void OnTriggerExit(Collider otherCollider)
+        {
+            if (CollisionDetectionManager.Instance == null)
+            {
+                return;
+            }
+
+            var view1 = this.gameObject.GetComponent<IView>();
+            var view2 = otherCollider.gameObject.GetComponent<IView>();
+            CollisionDetectionManager.Instance.HandleOnTriggerExit(view1, view2);
+        }
+    }
+}
---
+++ Assets/Scripts/Managers/CollisionDetectionManager.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Extensions;
+using UnityEngine;
+using Views;
+
+namespace Managers
+{
+    public class CollisionDetectionManager : BaseManager<CollisionDetectionManager>
+    {
+        public event Action<IView ,IView> TriggerEnter = delegate { };
+        public event Action<IView ,IView> TriggerExit = delegate { };
+
+        public event Action<IView ,IView> CollisionEnter = delegate { };
+        public event Action<IView ,IView> CollisionExit = delegate { };
+
+        public override void Initialize()
+        {
+        }
+        public override void UnInitialize()
+        {
+        }
+
+        public override void Subscribe()
+        {
+        }
+        public override void UnSubscribe()
+        {
+        }
+
+        public void HandleOnTriggerEnter(IView view1, IView view2)
+        {
+            if (view1 == null ||
+                view2 == null)
+            {
+                return;
+            }
+
+            Debug.Log($"{this.GetType().Name}.{ReflectionExtensions.GetCallerMemberName()}" +
+                      $"\n{nameof(view1)}={view1.GetType().Name}" +
+                      $"\n{nameof(view2)}={view2.GetType().Name}");
+
+            TriggerEnter.Invoke(view1, view2);
+        }
+        public void HandleOnTriggerExit(IView view1, IView view2)
+        {
+            if (view1 == null ||
+                view2 == null)
+            {
+                return;
+            }
+
+            Debug.Log($"{this.GetType().Name}.{ReflectionExtensions.GetCallerMemberName()}" +
+                      $"\n{nameof(view1)}={view1.GetType().Name}" +
+                      $"\n{nameof(view2)}={view2.GetType().Name}");
+
+            TriggerExit.Invoke(view1, view2);
+        }
+
+        public void HandleOnCollisionEnter(IView view1, IView view2)
+        {
+            Debug.Log($"{this.GetType().Name}.{ReflectionExtensions.GetCallerMemberName()}" +
+                      $"\n{nameof(view1)}={view1.GetType().Name}" +
+                      $"\n{nameof(view2)}={view2.GetType().Name}");
+
+            CollisionEnter.Invoke(view1, view2);
+        }
+        public void HandleOnCollisionExit(IView view1, IView view2)
+        {
+            Debug.Log($"{this.GetType().Name}.{ReflectionExtensions.GetCallerMemberName()}" +
+                      $"\n{nameof(view1)}={view1.GetType().Name}" +
+                      $"\n{nameof(view2)}={view2.GetType().Name}");
+
+            CollisionExit.Invoke(view1, view2);
+        }
+    }
+}
