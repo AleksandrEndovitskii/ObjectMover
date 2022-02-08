ef3cca82ca117384ba8d336f9c3c8f6b09976c17
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2022-02-09 00:00:55 +0300

    Add explosion of mainGameObject at collision with any game object

--- Assets/Scripts/Managers/ObjectsManager.cs
+++ Assets/Scripts/Managers/ObjectsManager.cs
@@ -1,7 +1,14 @@
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using DG.Tweening;
+using DG.Tweening.Core;
+using DG.Tweening.Plugins.Options;
+using Helpers;
 using UnityEngine;
+using Views;
+using Random = UnityEngine.Random;
 
 namespace Managers
 {
@@ -15,12 +22,17 @@ namespace Managers
         private GameObject _mainGameObjectPrefab;
         [SerializeField]
         private List<Vector3> _targetPoints = new List<Vector3>();
-        private List<Vector3> _tempTargetPoints = new List<Vector3>();
+        [SerializeField]
         private float _mainGameObjectMovementDuration = 1f;
+        [SerializeField]
+        private float _mainGameObjectMovementDelaySeconds = 3f;
+
+        private List<Vector3> _tempTargetPoints = new List<Vector3>();
 
         private List<GameObject> _gameObjectInstances = new List<GameObject>();
         private GameObject _mainGameObjectInstance;
         private readonly Vector3 MainGameObjectInitialPosition = new Vector3(0, 0, 0);
+        private TweenerCore<Vector3, Vector3, VectorOptions> _doLocalMove;
 
         private const int GAME_OBJECTS_RANDOM_POSITION_X_MIN = -10;
         private const int GAME_OBJECTS_RANDOM_POSITION_X_MAX = 10;
@@ -31,12 +43,6 @@ namespace Managers
 
         public override void Initialize()
         {
-            InstantiateGameObjects();
-
-            InstantiateMainGameObject();
-
-            _tempTargetPoints = new List<Vector3>(_targetPoints); // will be used in TryMoveToNextTargetPoint
-            TryMoveToNextTargetPoint();
         }
         public override void UnInitialize()
         {
@@ -44,6 +50,23 @@ namespace Managers
 
         public override void Subscribe()
         {
+        }
+        protected override async void SubscribeAsync()
+        {
+            await TaskHelper.WaitUntil(() => CollisionDetectionManager.Instance != null);
+
+            CollisionDetectionManager.Instance.TriggerEnter += CollisionDetectionManagerOnTriggerEnter;
+
+            InstantiateGameObjects();
+
+            InstantiateMainGameObject();
+
+            var mainGameObjectMovementCoroutine = StartCoroutine(InvokeActionAfterDelayCoroutine(() =>
+                {
+                    _tempTargetPoints = new List<Vector3>(_targetPoints); // will be used in TryMoveToNextTargetPoint
+                    TryMoveToNextTargetPoint();
+                },
+                _mainGameObjectMovementDelaySeconds));
         }
         public override void UnSubscribe()
         {
@@ -78,6 +101,10 @@ namespace Managers
 
         private void TryMoveToNextTargetPoint()
         {
+            if (_mainGameObjectInstance == null)
+            {
+                return;
+            }
             if (!_tempTargetPoints.Any())
             {
                 if (ExplosionManager.Instance != null)
@@ -90,9 +117,9 @@ namespace Managers
 
             var targetPoint = _tempTargetPoints.FirstOrDefault();
             _tempTargetPoints.Remove(targetPoint);
-            var doLocalMove = _mainGameObjectInstance.transform.DOLocalMove(
+            _doLocalMove = _mainGameObjectInstance.transform.DOLocalMove(
                 targetPoint, _mainGameObjectMovementDuration);
-            doLocalMove.OnComplete(TryMoveToNextTargetPoint);
+            _doLocalMove.OnComplete(TryMoveToNextTargetPoint);
         }
 
         private static Vector3 GetRandomPosition(int randomPositionXMin, int randomPositionXMax,
@@ -104,6 +131,33 @@ namespace Managers
             var randomPositionZ = Random.Range(randomPositionZMin, randomPositionZMax);
             var randomPosition = new Vector3(randomPositionX, randomPositionY, randomPositionZ);
             return randomPosition;
+        }
+
+        private void TryHandleVaultPlayerCollisionEnter(IView view1, IView view2)
+        {
+            var gameObjectView = view1 as GameObjectView;
+            var mainGameObjectView = view2 as MainGameObjectView;
+
+            if (gameObjectView == null ||
+                mainGameObjectView == null)
+            {
+                return;
+            }
+
+            _doLocalMove.Kill();
+            ExplosionManager.Instance.Explode(_mainGameObjectInstance);
+        }
+
+        private void CollisionDetectionManagerOnTriggerEnter(IView view1, IView view2)
+        {
+            TryHandleVaultPlayerCollisionEnter(view1, view2);
+        }
+
+        private IEnumerator InvokeActionAfterDelayCoroutine(Action action, float delaySeconds = 1f)
+        {
+            yield return new WaitForSeconds(delaySeconds);
+
+            action?.Invoke();
         }
     }
 }
