445cf7d530ed6c6d4fdae8bf06c1cd9fc0eb051d
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2022-02-08 19:43:58 +0300

    Add instantiating of game objects in random positions

--- Assets/Scripts/Managers/ObjectsManager.cs
+++ Assets/Scripts/Managers/ObjectsManager.cs
@@ -12,14 +12,37 @@ namespace Managers
 
         private List<GameObject> _gameObjectInstances = new List<GameObject>();
 
+        private const int GAME_OBJECTS_RANDOM_POSITION_X_MIN = -10;
+        private const int GAME_OBJECTS_RANDOM_POSITION_X_MAX = 10;
+        private const int GAME_OBJECTS_RANDOM_POSITION_Y_MIN = -10;
+        private const int GAME_OBJECTS_RANDOM_POSITION_Y_MAX = 10;
+        private const int GAME_OBJECTS_RANDOM_POSITION_Z_MIN = -10;
+        private const int GAME_OBJECTS_RANDOM_POSITION_Z_MAX = 10;
+
         public override void Initialize()
         {
             for (var i = 0; i < _gameObjectInstancesCount; i++)
             {
-                var gameObjectInstance = Instantiate(_gameObjectPrefab);
+                var randomPosition = GetRandomPosition(
+                    GAME_OBJECTS_RANDOM_POSITION_X_MIN, GAME_OBJECTS_RANDOM_POSITION_X_MAX,
+                    GAME_OBJECTS_RANDOM_POSITION_Y_MIN,GAME_OBJECTS_RANDOM_POSITION_Y_MAX,
+                    GAME_OBJECTS_RANDOM_POSITION_Z_MIN, GAME_OBJECTS_RANDOM_POSITION_Z_MAX);
+                var gameObjectInstance = Instantiate(_gameObjectPrefab, randomPosition, Quaternion.identity);
                 _gameObjectInstances.Add(gameObjectInstance);
             }
         }
+
+        private static Vector3 GetRandomPosition(int randomPositionXMin, int randomPositionXMax,
+            int randomPositionYMin, int randomPositionYMax,
+            int randomPositionZMin, int randomPositionZMax)
+        {
+            var randomPositionX = Random.Range(randomPositionXMin, randomPositionXMax);
+            var randomPositionY = Random.Range(randomPositionYMin, randomPositionYMax);
+            var randomPositionZ = Random.Range(randomPositionZMin, randomPositionZMax);
+            var randomPosition = new Vector3(randomPositionX, randomPositionY, randomPositionZ);
+            return randomPosition;
+        }
+
         public override void UnInitialize()
         {
         }
