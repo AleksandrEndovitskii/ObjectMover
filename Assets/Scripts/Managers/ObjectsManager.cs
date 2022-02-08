69869608804534109bc6a2fca1a2c2a0db2a515c
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2022-02-08 19:32:32 +0300

    Add instantiating of 100 game objects

--- Assets/Scripts/Managers/ObjectsManager.cs
+++ Assets/Scripts/Managers/ObjectsManager.cs
@@ -1,9 +1,24 @@
+using System.Collections.Generic;
+using UnityEngine;
+
 namespace Managers
 {
     public class ObjectsManager : BaseManager<ObjectsManager>
     {
+        [SerializeField]
+        private GameObject _gameObjectPrefab;
+        [SerializeField]
+        private int _gameObjectInstancesCount;
+
+        private List<GameObject> _gameObjectInstances = new List<GameObject>();
+
         public override void Initialize()
         {
+            for (var i = 0; i < _gameObjectInstancesCount; i++)
+            {
+                var gameObjectInstance = Instantiate(_gameObjectPrefab);
+                _gameObjectInstances.Add(gameObjectInstance);
+            }
         }
         public override void UnInitialize()
         {
